38ddaa21713604b3475ecdb031cf35931b133d17
elias <eliaslaramartinez@gmail.com>
2023-10-11 19:14:12 -0300

    herencia en poo javacript

--- Leccion07/Familia.js
+++ Leccion07/Familia.js
@@ -25,6 +25,10 @@ class Hija extends Padre{
 }
 
 let padre1 = new Padre('Juan','Lopez',44);
+let padre2 = new Padre('Pablo','Torres',55);
 let hija1 = new Hija('Ana','Sanchez',44,'anasanchez@gmail.com');
+let hija2 = new Hija('Camila','Lopez',66,'camila@gmail.com');
 console.log(padre1.obtenerDetalles());
 console.log(hija1.obtenerDetalles());
+console.log(padre2.obtenerDetalles());
+console.log(hija2.obtenerDetalles());
